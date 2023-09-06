6555ea5d435a2d95c4c8448b61665002e8e157cc
danghieu1407 <danghieu140701@gmail.com>
2023-09-06 21:46:04 +0700

    M4.2: StudentQuiz external functions: need to update for new base class #711068

--- classes/external/update_question_state.php
+++ classes/external/update_question_state.php
@@ -25,17 +25,18 @@
 
 namespace mod_studentquiz\external;
 
-defined('MOODLE_INTERNAL') || die();
-
-use external_api;
-use external_function_parameters;
-use external_single_structure;
-use external_value;
+use core_external\external_api;
+use core_external\external_function_parameters;
+use core_external\external_single_structure;
+use core_external\external_value;
 use mod_studentquiz\local\studentquiz_helper;
 use mod_studentquiz\local\studentquiz_question;
 
+defined('MOODLE_INTERNAL') || die();
+if (!class_exists('core_external\external_api')) {
+    require_once($CFG->libdir . '/externallib.php');
+}
 require_once($CFG->dirroot . '/mod/studentquiz/locallib.php');
-require_once($CFG->libdir . '/externallib.php');
 
 /**
  * Create update question state services implementation.
